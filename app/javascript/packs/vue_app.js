de599d7cea198dc2ff502cb5d352f7178eb92271
Andy Waite <andyw8@users.noreply.github.com>
2019-06-01 20:53:21 -0400

    Rename Vue app

--- app/javascript/packs/vue_app.js
+++ app/javascript/packs/vue_app.js
@@ -4,7 +4,7 @@ import Vue from 'vue/dist/vue.esm'
 import App from '../app.vue'
 
 document.addEventListener('DOMContentLoaded', () => {
-  const app = new Vue({
+  const vm = new Vue({
     el: '#hello',
     data: {
       message: "Can you say hello?"
