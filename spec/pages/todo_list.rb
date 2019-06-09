baa7156e36dac957e7406c1e0141c5aed3fab463
Andy Waite <andyw8@users.noreply.github.com>
2019-06-09 11:08:36 -0400

    Wait for XHR to prevent instability

--- spec/pages/todo_list.rb
+++ spec/pages/todo_list.rb
@@ -1,13 +1,17 @@
 require "sections/new_todo_item"
 require "sections/item"
+require "sections/item"
+require "pages/support/wait_for_xhr"
 
 module Pages
   class TodoList < SitePrism::Page
+    include WaitForXhr
     set_url '/'
     sections :items, Sections::Item, "li.todo-item"
     section :new_todo_item, Sections::NewTodoItem, '#new-todo-item'
 
     def item_titles
+      wait_for_xhr
       items.map(&:label_text)
     end
 
